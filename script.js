912c38fdeed9c79d6754a08485796f6c6bb29046
chasingdonis <cuixianning@hotmail.com>
2015-10-07 14:22:03 -0500

    edit js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-function paraPage() {
-    document.getElementById("exe").onclick = function() {
+ document.getElementById("exe").onclick = function() {
         if (document.getElementById("exe").className == "wrapper") {
             document.getElementById("exe").className = "";}
         else {
             document.getElementById("exe").className = "wrapper";
         }
     };
-}
-
-window.onload = function() {
-    paraPage();
-}
